4908168a93ac9a9b26f2114cf45cc04427c3dc7e
classickartik <73540552+classickartik@users.noreply.github.com>
2021-07-17 02:27:57 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+Array Partition I (https://leetcode.com/problems/array-partition-i/)
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        for(int i=0;i<nums.size()-1;i+=2){
+            cout<<nums[i];
+            sum+=min(nums[i],nums[i+1]);
+        }
+        return sum;
+    }
+};
