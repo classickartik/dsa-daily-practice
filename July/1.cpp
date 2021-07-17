6c10d70c0c99482a1cedb0db9d1165c1fb762579
classickartik <73540552+classickartik@users.noreply.github.com>
2021-07-17 16:52:20 +0530

    Update 1.cpp

--- July/1.cpp
+++ July/1.cpp
@@ -1,9 +1,7 @@
-[Best Time to Buy and Sell Stock II](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/)
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
+        int profit=0; 
         for(int i=1;i<prices.size();i++){
            if(prices[i]>prices[i-1] ){
                cout<<prices[i];
