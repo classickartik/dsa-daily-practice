c3e83296d2f64385871b3a82487bc23e3cc67b37
classickartik <73540552+classickartik@users.noreply.github.com>
2021-08-25 12:33:49 +0530

    Create 7.java

---
+++ August/7.java
@@ -0,0 +1,20 @@
+// Peak Index in a Mountain Array
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s=0;
+        int e = arr.length-1;
+        while(s<e){
+            int mid = s + (e-s)/2;
+            if(arr[mid]>arr[mid+1] ){
+                e=mid;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return s;
+    }
+
+}
+    
