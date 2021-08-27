bbc8bcc6998872be04d4af675c1e3b11c9464d34
classickartik <73540552+classickartik@users.noreply.github.com>
2021-08-27 21:23:25 +0530

    Create 11.java

---
+++ August/11.java
@@ -0,0 +1,22 @@
+// Search Insert Position
+
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length -1;
+        while(s<=e){
+          int mid = s + (e-s)/2;
+          if(target>arr[mid]){
+            s=mid+1;
+          }
+          else if(target<arr[mid]){
+            e=mid-1;
+          }
+          else{
+            return mid;
+          }
+          
+        }
+      return s;
+    }
+}
