bcd5494a26d0566c7ea7fda4d01b752afbac3216
classickartik <73540552+classickartik@users.noreply.github.com>
2021-09-18 02:44:37 +0530

    Create 12.java

---
+++ September/12.java
@@ -0,0 +1,26 @@
+//287. Find the Duplicate Number
+
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while(i<arr.length){
+            int correctindex = arr[i]-1;
+            if(arr[correctindex]!=arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[correctindex];
+                arr[correctindex] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        for(int j = 0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                return arr[j];
+            }    
+        }
+        // System.out.print(Arrays.toString(arr));
+        return 0;
+    }
+}
