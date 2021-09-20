bb7283a21b7d16e819e31dd7e4875dc97e670e53
classickartik <73540552+classickartik@users.noreply.github.com>
2021-09-21 01:18:34 +0530

    Create 15.java

---
+++ September/15.java
@@ -0,0 +1,24 @@
+//41. First Missing Positive
+
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i]<=arr.length && arr[i]>0 && arr[i]!=arr[correct]){
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j = 0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                return j+1;
+            }
+        }
+        return arr.length+1;
+    }
+}
